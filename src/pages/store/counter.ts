c25a8a91231f7b6e0e7625536f6ac28f5dff67ff
NishatFariza <nishatfarzia78@gmail.com>
2022-10-20 03:15:53 +0600

    added redux dispatch action

--- src/pages/store/counter.ts
+++ src/pages/store/counter.ts
@@ -1,18 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
+interface CounterState {
+    count :number,
+}
 
 
 const countSlice = createSlice({
     name: "counter",
-    initialState: 0 ,
+    initialState: {
+        count: 0,
+    } ,
     reducers: {
-        increment(state: number) {
-            state++;
+        increment(state: CounterState) {
+            state.count +=1;
         },
 
-        decrement(state: number) {
-            state--;
+        decrement(state: CounterState) {
+            state.count -=1;
         }
      }
 })
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment } from "./store/counter";
+import { decrement, increment } from "./store/counter";
 
 const TestPage = () => {
+  const counter = useSelector((store: any) => store.counter.count);
 
-  const counter = useSelector((store: any) => store.counter)
-
-  const dispatch = useDispatch()
-
+  const dispatch = useDispatch();
 
   return (
     <div>
-      <button onClick={() => dispatch(increment())}>-</button>
-      <button>+</button>
-      <h2>Count: { counter}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <h2>Count: {counter}</h2>
     </div>
   );
 };
