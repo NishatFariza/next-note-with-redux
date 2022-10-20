22fdb64caac776e8bf511f7d56e1a7ad21e7c953
NishatFariza <nishatfarzia78@gmail.com>
2022-10-20 08:00:38 +0600

    style: note color

--- src/pages/store/notes.ts
+++ src/pages/store/notes.ts
@@ -13,29 +13,36 @@ const initialState: NoteState = {
         title: "Note 1",
         content: "Content 1",
         createAt: "2022-01-01",
-        color: "red"
+        color: "#d8e2dc"
         },
         {
         id: "2",
         title: "Note 2",
         content: "Content 2",
         createAt: "2022-01-02",
-        color: "green"
+        color: "#ffe5d9"
         },
         {
         id: "3",
         title: "Note 3",
         content: "Content 3",
         createAt: "2022-01-03",
-        color: "blue"
+        color: "#fbfaf0"
         },
         {
         id: "4",
         title: "Note 4",
         content: "Content 4",
         createAt: "2022-01-04",
-        color: "teal"
-        }
+        color: "#ffdde4"
+        },
+        {
+        id: "5",
+        title: "Note 5",
+        content: "Content 5",
+        createAt: "2022-01-05",
+        color: "#ffe9ee"
+        },
     ],
 };
 
